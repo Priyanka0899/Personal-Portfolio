e661e466dd0ff0f6c47752b9e870d4b75b069261
Priyanka Arora <parora0711@gmail.com>
2024-08-06 21:39:18 +0530

    changes

--- Priyanka-Portfolio/src/data/constants.js
+++ Priyanka-Portfolio/src/data/constants.js
@@ -8,7 +8,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/Priyanka0899",
   resume:
-    "https://drive.google.com/file/d/1MnQoCV5r2bXwHi5QZueWs-6vNfLgG8DN/view?usp=sharing",
+    "https://drive.google.com/file/d/1Zyzd33gGmcadqzdvvmZ_tPQTiaC_91rM/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/priyanka-arora08/",
   insta: "https://www.instagram.com/_pirriii/",
 };
